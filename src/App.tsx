c4478039116d56d8a6241307c2b996139b6cd672
VitinhoSouza <vitinhosouzajuatama@gmail.com>
2023-01-21 11:41:10 -0300

    Class 4 - Connecting to the API

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { Header } from "./components/Header";
 import { SummaryTable } from "./components/SummaryTable";
 
-import './styles/global.css';
+import "./styles/global.css";
+import "./lib/dayjs";
 
 export function App() {
 
